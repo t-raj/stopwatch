a82dcb61922baeffc9557d76cb304eb93ab63ead
emmahighland <ehighland@luc.edu>
2015-04-07 15:00:02 -0500

    Updates...some improvements!

---
+++ Stopwatch/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/IncrementingState.java
@@ -0,0 +1,38 @@
+package edu.luc.etl.cs313.android.simplestopwatch.model.state;
+
+import edu.luc.etl.cs313.android.simplestopwatch.R;
+
+/**
+ * Created by emmahighland on 4/7/15.
+ */
+class IncrementingState implements StopwatchState {
+
+    public IncrementingState(final StopwatchSMStateView sm) {
+        this.sm = sm;
+    }
+
+    private final StopwatchSMStateView sm;
+
+    @Override
+    public void onSetReset() { //switch to decrementing after 3 seconds
+        int time = this.getId();
+        if (time == 3 || time == 99) {
+            sm.toDecrementingState();
+        }
+    }
+
+    @Override
+    public void onTick() {
+        throw new UnsupportedOperationException("onTick");
+    }
+
+    @Override
+    public void updateView() {
+        sm.updateUIRuntime();
+    }
+
+    @Override
+    public int getId() {
+        return R.string.INCREMENTING;
+    }
+}
